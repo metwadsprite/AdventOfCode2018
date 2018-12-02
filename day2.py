2fb184d2bbcce18271ab784f0742773003569c3a
Vlad Vasilescu <vladvas98@yahoo.com>
2018-12-03 00:33:09 +0200

    Day 2

--- day2.py
+++ day2.py
@@ -1,46 +1,52 @@
-file = open("input.txt", "r")
+def p1(file):
+    alph = 'abcdefghijklmnopqrstuvwxyz'
 
-# alph = 'abcdefghijklmnopqrstuvwxyz'
-#
-# no_two = 0
-# no_three = 0
-#
-# for line in file:
-#     add_two = False
-#     add_three = False
-#
-#     for char in alph:
-#         if line.count(char) == 2 and not add_two:
-#             no_two += 1
-#             add_two = True
-#         elif line.count(char) == 3 and not add_three:
-#             no_three += 1
-#             add_three = True
-#
-# print(no_two * no_three)
+    no_two = 0
+    no_three = 0
 
-lines = []
+    for line in file:
+        add_two = False
+        add_three = False
 
-for line in file:
-    lines.append(line.strip())
+        for char in alph:
+            if line.count(char) == 2 and not add_two:
+                no_two += 1
+                add_two = True
+            elif line.count(char) == 3 and not add_three:
+                no_three += 1
+                add_three = True
 
-file.close()
+    print(no_two * no_three)
+    return
 
-dif_list = []
 
-for line1 in lines:
-    for line2 in lines:
-        dif_no = 0
+def p2(file):
+    lines = []
 
-        for it in range(0, len(line1)):
-            if line1[it] != line2[it]:
-                dif_no += 1
-                dif_it = it
+    for line in file:
+        lines.append(line.strip())
 
-        if dif_no == 1:
+    dif_list = []
+
+    for line1 in lines:
+        for line2 in lines:
+            dif_no = 0
+
             for it in range(0, len(line1)):
-                if it != dif_it:
-                    dif_list.append(line1[it])
+                if line1[it] != line2[it]:
+                    dif_no += 1
+                    dif_it = it
 
-            print(*dif_list, sep="")
-            exit()
+            if dif_no == 1:
+                for it in range(0, len(line1)):
+                    if it != dif_it:
+                        dif_list.append(line1[it])
+
+                print(*dif_list, sep="")
+                return
+
+
+file = open("input.txt", "r")
+# p1(file)
+p2(file)
+file.close()
