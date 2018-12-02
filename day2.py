a78baadb28a7eba24082a3f8b979e29f9d1e6999
Vlad Vasilescu <vladvas98@yahoo.com>
2018-12-02 13:02:22 +0200

    Day 2

---
+++ day2.py
@@ -0,0 +1,47 @@
+file = open("input.txt", "r")
+
+# alph = 'abcdefghijklmnopqrstuvwxyz'
+#
+# no_two = 0
+# no_three = 0
+#
+# for line in file:
+#     add_two = False
+#     add_three = False
+#
+#     for char in alph:
+#         if line.count(char) == 2 and not add_two:
+#             no_two += 1
+#             add_two = True
+#         elif line.count(char) == 3 and not add_three:
+#             no_three += 1
+#             add_three = True
+#
+# print(no_two * no_three)
+
+lines = []
+
+for line in file:
+    lines.append(line.strip())
+
+file.close()
+
+dif_set = set()
+dif_list = []
+
+for line1 in lines:
+    for line2 in lines:
+        dif_no = 0
+
+        for it in range(0, len(line1)):
+            if line1[it] != line2[it]:
+                dif_no += 1
+                dif_it = it
+
+        if dif_no == 1:
+            for it in range(0, len(line1)):
+                if it != dif_it:
+                    dif_list.append(line1[it])
+
+            print(*dif_list, sep="")
+            exit()
