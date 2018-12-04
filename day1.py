6a375dc35db4c50365506428618c55c82e74369c
Vlad Vasilescu <vladvas98@yahoo.com>
2018-12-04 15:25:58 +0200

    Day 1

--- day1.py
+++ day1.py
@@ -1,19 +1,37 @@
-freq = 0
-prev_freq = set()
-prev_freq.add(0)
+def p1(file):
+    freq = 0
 
-file = open("input.txt", "r")
-op = []
-for line in file:
-    op.append(line)
-file.close()
+    op = []
+    for line in file:
+        op.append(line)
 
-while True:
     for line in op:
         freq += int(line)
 
-        if freq in prev_freq:
-            print(freq)
-            exit()
+    print(freq)
 
-        prev_freq.add(freq)
+
+def p2(file):
+    freq = 0
+    prev_freq = set()
+    prev_freq.add(0)
+
+    op = []
+    for line in file:
+        op.append(line)
+
+    while True:
+        for line in op:
+            freq += int(line)
+
+            if freq in prev_freq:
+                print(freq)
+                exit()
+
+            prev_freq.add(freq)
+
+
+file = open("input.txt", "r")
+# p1(file)
+p2(file)
+file.close()
