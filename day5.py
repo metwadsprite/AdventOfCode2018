5959e9a4eca4743f384f11618bd5d5016e71d2ce
Vlad Vasilescu <vladvas98@yahoo.com>
2018-12-05 17:53:20 +0200

    Day 5

--- day5.py
+++ day5.py
@@ -1,23 +1,18 @@
+import time
+
 def p1(file):
     poly = file.readline()
-    lowercase = 'abcdefghijklmnopqrstuvwxyz'
-    uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
     it = 0
     while it != len(poly) - 1:
-        if poly[it] in lowercase:
-            if poly[it + 1] in uppercase:
-                if ord(poly[it]) == ord(poly[it + 1]) + 32:
-                    poly = poly[:it] + poly[it + 2:]
-                    it -= 1
-                    continue
-        if poly[it] in uppercase:
-            if poly[it + 1] in lowercase:
-                if ord(poly[it]) == ord(poly[it + 1]) - 32:
-                    poly = poly[:it] + poly[it + 2:]
-                    it -= 1
-                    continue
-        it += 1
+        if ord(poly[it]) == ord(poly[it + 1]) + 32:
+            poly = poly[:it] + poly[it + 2:]
+            it -= 1
+        elif ord(poly[it]) == ord(poly[it + 1]) - 32:
+            poly = poly[:it] + poly[it + 2:]
+            it -= 1
+        else:
+            it += 1
 
     print(len(poly) - 1)
 
@@ -27,39 +22,35 @@ def p2(file):
     lowercase = 'abcdefghijklmnopqrstuvwxyz'
     uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    poly_bk = poly
-    lens = []
+    poly_main = poly
+    lenghts = []
 
-    for lit in range(0, len(lowercase)):
-        poly = poly_bk
-        if lowercase[lit] not in poly and uppercase[lit] not in poly:
+    for alph_it in range(0, len(lowercase)):
+        poly = poly_main
+
+        if lowercase[alph_it] not in poly and uppercase[alph_it] not in poly:
             continue
 
-        pit = 0
-        while pit != len(poly):
-            if poly[pit] == lowercase[lit] or poly[pit] == uppercase[lit]:
-                poly = poly[:pit] + poly[pit + 1:]
-                pit -= 1
-            pit += 1
+        it = 0
+        while it != len(poly):
+            if poly[it] == lowercase[alph_it] or poly[it] == uppercase[alph_it]:
+                poly = poly[:it] + poly[it + 1:]
+                it -= 1
+            it += 1
 
         it = 0
         while it != len(poly) - 1:
-            if poly[it] in lowercase:
-                if poly[it + 1] in uppercase:
-                    if ord(poly[it]) == ord(poly[it + 1]) + 32:
-                        poly = poly[:it] + poly[it + 2:]
-                        it -= 1
-                        continue
-            if poly[it] in uppercase:
-                if poly[it + 1] in lowercase:
-                    if ord(poly[it]) == ord(poly[it + 1]) - 32:
-                        poly = poly[:it] + poly[it + 2:]
-                        it -= 1
-                        continue
-            it += 1
+            if ord(poly[it]) == ord(poly[it + 1]) + 32:
+                poly = poly[:it] + poly[it + 2:]
+                it -= 1
+            elif ord(poly[it]) == ord(poly[it + 1]) - 32:
+                poly = poly[:it] + poly[it + 2:]
+                it -= 1
+            else:
+                it += 1
 
-        lens.append(len(poly) - 1)
-    print(min(lens))
+        lenghts.append(len(poly) - 1)
+    print(min(lenghts))
 
 
 file = open('input.txt', 'r')
